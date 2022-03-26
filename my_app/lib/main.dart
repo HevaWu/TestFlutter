5101bc5819e6fe5e77c0161603048ddd3b906837
He Wu <hewu9375@gmail.com>
2022-03-26 23:58:49 +0900

    try StatefulWidget

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -11,19 +11,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        ),
+        body: const Center(child: RandomWords()),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWidgetState();
+}
+
+class _RandomWidgetState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
 
