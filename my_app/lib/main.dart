e47276a6cad65002311632188ae1834b8ce66e22
He Wu <hewu9375@gmail.com>
2022-03-27 00:14:39 +0900

    generate infinite list, lazily load of words when user scroll down

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -12,13 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(child: RandomWords()),
-      ),
+      title: "Startup Name Generateor",
+      home: RandomWords(),
     );
   }
 }
@@ -31,10 +26,32 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWidgetState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Startup Name Generator"),
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return ListTile(
+            title: Text(
+              _suggestions[index].asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        },
+      ),
+    );
   }
 }
 
