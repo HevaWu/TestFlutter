fb4dacbcea0e648be1dc9dff8ce5f0b832a0094a
he.wu <he.wu@woven-planet.global>
2022-04-01 16:56:21 +0900

    init

---
+++ fade_a_widget_in_and_out/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// ignore this for now
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: Text('Flutter Demo Home Page'),
+    );
+  }
+}
