aeb9f1f9303170fccec2bd0c8a79c5715799fd94
he.wu <he.wu@woven-planet.global>
2022-04-01 15:28:14 +0900

    set up animation controller

--- animate_a_widget_using_a_physics_simulation/lib/main.dart
+++ animate_a_widget_using_a_physics_simulation/lib/main.dart
@@ -1,9 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: PhysicsCardDragDemo(),
+  ));
 }
 
+class PhysicsCardDragDemo extends StatelessWidget {
+  const PhysicsCardDragDemo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const DraggableCard(
+        child: FlutterLogo(size: 128),
+      ),
+    );
+  }
+}
+
+class DraggableCard extends StatefulWidget {
+  const DraggableCard({required this.child, Key? key}) : super(key: key);
+  final Widget child;
+
+  @override
+  State<DraggableCard> createState() => _DraggableCardState();
+}
+
+class _DraggableCardState extends State<DraggableCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      child: Card(child: widget.child),
+    );
+  }
+}
+
+// Use for test, ignore it now
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
