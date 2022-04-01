8737b194904155ec2ea1d694c5586af2c10bc8ef
he.wu <he.wu@woven-planet.global>
2022-04-01 14:55:11 +0900

    create animation with easing curve

--- animate_a_page_route_transition/lib/main.dart
+++ animate_a_page_route_transition/lib/main.dart
@@ -31,11 +31,12 @@ Route _createRoute() {
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
       const begin = Offset(0.0, 1.0);
       const end = Offset.zero;
-      final tween = Tween(begin: begin, end: end);
-      final offsetAnimation = animation.drive(tween);
+      var curve = Curves.ease;
+      final tween =
+          Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
       return SlideTransition(
-        position: offsetAnimation,
+        position: animation.drive(tween),
         child: child,
       );
     },
