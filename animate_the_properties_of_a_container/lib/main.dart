a1e77108563cead0c37a9a0ae9c6a1c6f5a59554
he.wu <he.wu@woven-planet.global>
2022-04-01 16:49:05 +0900

    start animation by rebuilding with new properties

--- animate_the_properties_of_a_container/lib/main.dart
+++ animate_the_properties_of_a_container/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,20 +14,42 @@ class AnimatedContainerApp extends StatefulWidget {
 }
 
 class _AnimatedContainerAppState extends State<AnimatedContainerApp> {
-  final double _width = 50;
-  final double _height = 50;
-  final Color _color = Colors.green;
-  final BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.green;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      width: _width,
-      height: _height,
-      decoration: BoxDecoration(color: _color, borderRadius: _borderRadius),
-      duration: const Duration(seconds: 1),
-      curve: Curves.fastOutSlowIn,
-    );
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: AnimatedContainer(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(color: _color, borderRadius: _borderRadius),
+          duration: const Duration(seconds: 1),
+          curve: Curves.fastOutSlowIn,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            final random = Random();
+            _width = random.nextInt(300).toDouble();
+            _height = random.nextInt(300).toDouble();
+
+            _color = Color.fromRGBO(random.nextInt(256), random.nextInt(256),
+                random.nextInt(256), 1);
+
+            _borderRadius =
+                BorderRadius.circular(random.nextInt(100).toDouble());
+          });
+        },
+        child: const Icon(Icons.play_arrow),
+      ),
+    ));
   }
 }
 
