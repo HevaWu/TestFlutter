afadc46abfd65bd69e448316b71671409ca15c73
he.wu <he.wu@woven-planet.global>
2022-04-01 17:06:48 +0900

    AnimatedOpacity to fade box in and out

--- fade_a_widget_in_and_out/lib/main.dart
+++ fade_a_widget_in_and_out/lib/main.dart
@@ -4,6 +4,46 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _visible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0,
+          duration: const Duration(milliseconds: 500),
+          child: Container(
+            width: 200.0,
+            height: 200.0,
+            color: Colors.green,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+        tooltip: 'Toggle Opacity',
+        child: const Icon(Icons.flip),
+      ),
+    );
+  }
+}
+
 // ignore this for now
 
 class MyApp extends StatelessWidget {
@@ -14,7 +54,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: Text('Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Opacity Demo'),
     );
   }
 }
