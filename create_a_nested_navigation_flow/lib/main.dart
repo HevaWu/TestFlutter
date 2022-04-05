a11916ad11008a4120819cbc85fbed5f8f2cf831
he.wu <he.wu@woven-planet.global>
2022-04-05 16:25:45 +0900

    add all related pages design and register route

--- create_a_nested_navigation_flow/lib/main.dart
+++ create_a_nested_navigation_flow/lib/main.dart
@@ -1,7 +1,294 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+        ),
+        floatingActionButtonTheme:
+            const FloatingActionButtonThemeData(backgroundColor: Colors.blue),
+      ),
+      onGenerateRoute: (settings) {
+        late Widget page;
+
+        if (settings.name == routeHome) {
+          page = const HomeScreen();
+        } else if (settings.name == routeSettings) {
+          page = const SettingsScreen();
+        } else if (settings.name!.startsWith(routePrefixDeviceSetup)) {
+          final subRoute =
+              settings.name!.substring(routePrefixDeviceSetup.length);
+          page = SetupFlow(
+            setupPageRoute: subRoute,
+          );
+        } else {
+          throw Exception('Unknown route: ${settings.name}');
+        }
+
+        return MaterialPageRoute<dynamic>(
+          builder: (context) {
+            return page;
+          },
+          settings: settings,
+        );
+      },
+    ),
+  );
+}
+
+@immutable
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(context),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: 250,
+                height: 250,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xFF222222),
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.lightbulb,
+                    size: 175,
+                    color: Theme.of(context).scaffoldBackgroundColor,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+              const Text(
+                'Add your First Bulb',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed(routeDeviceSetupStart);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar(BuildContext context) {
+    return AppBar(
+      title: const Text('Welcome'),
+      actions: [
+        IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, routeSettings);
+            },
+            icon: const Icon(Icons.settings))
+      ],
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: SingleChildScrollView(
+          child: Column(
+        children: List.generate(
+          8,
+          (index) {
+            return Container(
+              width: double.infinity,
+              height: 54,
+              margin: const EdgeInsets.only(right: 16, left: 16, top: 16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: const Color(0xFF222222),
+              ),
+            );
+          },
+        ),
+      )),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      title: const Text('Settings'),
+    );
+  }
+}
+
+class SelectDevicePage extends StatelessWidget {
+  const SelectDevicePage({Key? key, required this.onDeviceSelected})
+      : super(key: key);
+
+  final void Function(String deviceId) onDeviceSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Select a nearby device',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            const SizedBox(height: 24),
+            SizedBox(
+              width: double.infinity,
+              height: 54,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateColor.resolveWith((states) {
+                    return const Color(0xFF222222);
+                  }),
+                ),
+                onPressed: () {
+                  onDeviceSelected('22n483nk5834');
+                },
+                child: const Text(
+                  'Bulb 22n483nk5834',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+class WaitingPage extends StatefulWidget {
+  const WaitingPage(
+      {Key? key, required this.message, required this.onWaitComplete})
+      : super(key: key);
+
+  final String message;
+  final VoidCallback onWaitComplete;
+
+  @override
+  State<WaitingPage> createState() => _WaitingPageState();
+}
+
+class _WaitingPageState extends State<WaitingPage> {
+  @override
+  void initState() {
+    super.initState();
+    _startWaiting();
+  }
+
+  Future<void> _startWaiting() async {
+    await Future<dynamic>.delayed(const Duration(seconds: 3));
+
+    if (mounted) {
+      widget.onWaitComplete();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24.0,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const CircularProgressIndicator(),
+            const SizedBox(height: 32),
+            Text(widget.message),
+          ],
+        ),
+      ),
+    ));
+  }
+}
+
+class FinishedPage extends StatelessWidget {
+  const FinishedPage({Key? key, required this.onFinishPressed})
+      : super(key: key);
+
+  final VoidCallback onFinishPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: 250,
+                height: 250,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xFF222222),
+                ),
+                child: const Center(
+                  child: Icon(
+                    Icons.lightbulb,
+                    size: 175,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+              const Text(
+                'Bulb added!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                  style: ButtonStyle(
+                      padding: MaterialStateProperty.resolveWith((states) {
+                    return const EdgeInsets.symmetric(
+                        horizontal: 24, vertical: 12);
+                  }), backgroundColor:
+                          MaterialStateProperty.resolveWith((states) {
+                    return const Color(0xFF222222);
+                  }), shape: MaterialStateProperty.resolveWith((states) {
+                    return const StadiumBorder();
+                  })),
+                  onPressed: onFinishPressed,
+                  child: const Text(
+                    'Finish',
+                    style: TextStyle(fontSize: 24),
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 const routeHome = '/';
@@ -12,24 +299,6 @@ const routeDeviceSetupStartPage = 'find_devices';
 const routeDeviceSetupSelectDevicePage = 'select_device';
 const routeDeviceSetupConnectingPage = 'connecting';
 const routeDeviceSetupFinishedPage = 'finished';
-
-// onGenerateRoute: (settings) {
-//   late Widget page;
-//   if (settings.name == routeHome) {
-//     page = const HomeScreen();
-//   } else if (settings.name == routeSettings) {
-//     page = const SettingsScreen();
-//   } else if (settings.name!.startWith(routePrefixDeviceSetup)) {
-//     final subRoute = settings.name!.substring(routePrefixDeviceSetup.length);
-//     page = SetupFlow(setupPageRoute: subRoute,);
-//   } else {
-//     throw Exception('Unknown route: ${settings.name}');
-//   }
-
-//   return MaterialPageRoute<dynamic>(builder: (context) {
-//     return page;
-//   }, settings: settings);
-// }
 
 class SetupFlow extends StatefulWidget {
   const SetupFlow({Key? key, required this.setupPageRoute}) : super(key: key);
@@ -46,9 +315,65 @@ class _SetupFlowState extends State<SetupFlow> {
     return WillPopScope(
         child: Scaffold(
           appBar: _buildFlowAppBar(),
-          body: const SizedBox(),
+          body: Navigator(
+            key: _navigatorKey,
+            initialRoute: widget.setupPageRoute,
+            onGenerateRoute: _onGenerateRoute,
+          ),
         ),
         onWillPop: _isExitDesired);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  final _navigatorKey = GlobalKey<NavigatorState>();
+
+  void _onDiscoveryComplete() {
+    _navigatorKey.currentState!.pushNamed(routeDeviceSetupSelectDevicePage);
+  }
+
+  void _onDeviceSelected(String deviceId) {
+    _navigatorKey.currentState!.pushNamed(routeDeviceSetupConnectingPage);
+  }
+
+  void _onConnectionEstablished() {
+    _navigatorKey.currentState!.pushNamed(routeDeviceSetupFinishedPage);
+  }
+
+  void _exitSetup() {
+    Navigator.of(context).pop();
+  }
+
+  Route _onGenerateRoute(RouteSettings settings) {
+    late Widget page;
+    switch (settings.name) {
+      case routeDeviceSetupStartPage:
+        page = WaitingPage(
+          message: 'Searching for nearby bulb...',
+          onWaitComplete: _onDiscoveryComplete,
+        );
+        break;
+      case routeDeviceSetupSelectDevicePage:
+        page = SelectDevicePage(onDeviceSelected: _onDeviceSelected);
+        break;
+      case routeDeviceSetupConnectingPage:
+        page = WaitingPage(
+            message: 'Connecting', onWaitComplete: _onConnectionEstablished);
+        break;
+      case routeDeviceSetupFinishedPage:
+        page = FinishedPage(onFinishPressed: _exitSetup);
+        break;
+    }
+
+    return MaterialPageRoute<dynamic>(
+      builder: (context) {
+        return page;
+      },
+      settings: settings,
+    );
   }
 
   PreferredSizeWidget _buildFlowAppBar() {
@@ -61,8 +386,11 @@ class _SetupFlowState extends State<SetupFlow> {
     );
   }
 
-  void _onExitPressed() {
-    Navigator.of(context).pop();
+  void _onExitPressed() async {
+    final isConfirmed = await _isExitDesired();
+    if (isConfirmed && mounted) {
+      _exitSetup();
+    }
   }
 
   Future<bool> _isExitDesired() async {
@@ -90,115 +418,5 @@ class _SetupFlowState extends State<SetupFlow> {
               );
             }) ??
         false;
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
   }
 }
