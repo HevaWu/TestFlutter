68428c0a4f9b0a9cc53d6c9e5d9a8a80cc5cf889
He Wu <hewu9375@gmail.com>
2022-04-06 20:49:30 +0900

    add FilterItem design, will use colorBlendMode

--- create_a_photo_filter_carousel/lib/main.dart
+++ create_a_photo_filter_carousel/lib/main.dart
@@ -82,6 +82,36 @@ class _FilterSelectorState extends State<FilterSelector> {
   }
 }
 
+@immutable
+class FilterItem extends StatelessWidget {
+  const FilterItem({
+    Key? key,
+    required this.color,
+    this.onFilterSelected,
+  }) : super(key: key);
+
+  final Color color;
+  final VoidCallback? onFilterSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onFilterSelected,
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: ClipOval(
+          child: Image.network(
+            'https://docs.flutter.dev/cookbook/img-files'
+            '/effects/instagram-buttons/millenial-texture.jpg',
+            color: color.withOpacity(0.5),
+            colorBlendMode: BlendMode.hardLight,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
