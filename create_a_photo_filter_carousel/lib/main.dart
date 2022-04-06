90b2433a3b4cb2154496d010f3399ecdb9c23870
He Wu <hewu9375@gmail.com>
2022-04-06 21:39:09 +0900

    update whole page design

--- create_a_photo_filter_carousel/lib/main.dart
+++ create_a_photo_filter_carousel/lib/main.dart
@@ -1,14 +1,72 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: FilterSelection(),
+  ));
 }
 
-// put FilterSelector on top of the photo, at the bottom and centered
-// Stack(children: [
-//   Positioned.fill(child: _buldPhotoWithFilter(),),
-//   const Positioned(left: 0.0, right: 0.0, bottom: 0.0, child: FilterSelector(),)
-// ]),
+@immutable
+class FilterSelection extends StatefulWidget {
+  const FilterSelection({Key? key}) : super(key: key);
+
+  @override
+  State<FilterSelection> createState() => _FilterSelectionState();
+}
+
+class _FilterSelectionState extends State<FilterSelection> {
+  final _filters = [
+    Colors.white,
+    ...List.generate(Colors.primaries.length,
+        (index) => Colors.primaries[(index * 4) % Colors.primaries.length]),
+  ];
+
+  final _filterColor = ValueNotifier<Color>(Colors.white);
+
+  void _onFilterChanged(Color value) {
+    _filterColor.value = value;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.black,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: _buildPhotoWithFilter(),
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: _buildFilterSelecter(),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPhotoWithFilter() {
+    return ValueListenableBuilder(
+      valueListenable: _filterColor,
+      builder: (context, value, child) {
+        final color = value as Color;
+        return Image.network(
+          'https://docs.flutter.dev/cookbook/'
+          'img-files/effects/instagram-buttons/millenial-dude.jpg',
+          color: color.withOpacity(0.5),
+          colorBlendMode: BlendMode.color,
+          fit: BoxFit.cover,
+        );
+      },
+    );
+  }
+
+  Widget _buildFilterSelecter() {
+    return FilterSelector(filters: _filters, onFilterChanged: _onFilterChanged);
+  }
+}
 
 @immutable
 class FilterSelector extends StatefulWidget {
@@ -227,6 +285,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// ignore bellow
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
