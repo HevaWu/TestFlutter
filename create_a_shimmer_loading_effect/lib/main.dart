de69c600252cd038e1e9e31c3f6af24032c539d4
he.wu <he.wu@woven-planet.global>
2022-04-07 17:06:34 +0900

    add top circleItem design

--- create_a_shimmer_loading_effect/lib/main.dart
+++ create_a_shimmer_loading_effect/lib/main.dart
@@ -4,6 +4,35 @@ void main() {
   runApp(const MyApp());
 }
 
+class CircleListItem extends StatelessWidget {
+  const CircleListItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8,
+        vertical: 8,
+      ),
+      child: Container(
+        width: 54,
+        height: 54,
+        decoration:
+            const BoxDecoration(color: Colors.black, shape: BoxShape.circle),
+        child: ClipOval(
+          child: Image.network(
+            'https://flutter'
+            '.dev/docs/cookbook/img-files/effects/split-check/Avatar1.jpg',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// ignore bellow
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
